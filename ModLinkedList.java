0a5ca013423dd7c8c331a66b56e6462742b235f7
Orhan Aytekin <contactorhanaytekin@gmail.com>
2022-06-24 11:18:45 +0300

    new Funciton AddFirst adds the element to the beginning of the list

---
+++ ModLinkedList.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+//my implementation of Linked List
+public class ModLinkedList {
+    Node root;
+
+    //Node class
+    static class Node{
+        Node next;
+        int data;
+
+        Node (int d){ data = d;}
+    }
+
+    //generates random Linked List with it's bound and length chosen by the user
+    static ModLinkedList GenerateLinkedList(int bound, int length){
+        Random random = new Random();
+        int root = random.nextInt(bound);
+        ModLinkedList modLinkedList = new ModLinkedList();
+        modLinkedList.root = new Node(root);
+        for (int i = 0; i<length-1; i++){
+            int a = random.nextInt(bound);
+            modLinkedList.AddNode(a);
+        }
+        return modLinkedList;
+    }
+
+    //Prints the Linked List
+    public void PrintList(){
+        Node temp = root;
+        while (root != null){
+            System.out.println(root.data);
+            root = root.next;
+        }
+        root = temp;
+    }
+
+    //new Node
+    public Node getNode(int a){
+        return new Node(a);
+    }
+
+    //add node at the end of the list
+    public void AddNode(int d){
+        Node toAdd = getNode(d);
+        Node temp = root;
+        while (root.next != null){
+            root = root.next;
+        }
+        root.next = toAdd;
+        root = temp;
+    }
+
+    //add node at the start of the list
+    // (EXTRA) ***add all the items from another list by using this O(1) constant time
+    // and you get a LinkedList with reverse order of the original list***
+    public void AddFirst(int d){
+        Node temp = root;
+        root = new Node(d);
+        root.next = temp;
+    }
+}
